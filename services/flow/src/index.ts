c61b113ce453d562a4d3e410aa06544dd5fa006b
ahmethalac <ahmetfeyzihalac@gmail.com>
2022-02-25 21:11:00 +0300

    initial flow service

---
+++ services/flow/src/index.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import { json } from 'body-parser';
+
+const PORT = process.env.PORT;
+const app = express();
+app.use(json());
+
+app.listen(PORT, () => {
+  console.log(`Flow management microservice is listening on port ${PORT}`);
+})
