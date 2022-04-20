5f1f807088aa0b3e3c551aaa66646c569a6047c3
Aybars Altinisik <aybars.altinisik@gmail.com>
2022-04-21 00:02:54 +0300

    HTTPS/SSL :: bugfix.

--- common/services/apiService.ts
+++ common/services/apiService.ts
@@ -4,13 +4,14 @@ import { SERVICES } from '../constants/services';
 // eslint-disable-next-line no-unused-vars
 type ApiService = AxiosInstance & { useService: (service: SERVICES) => AxiosInstance }
 export const apiService = Axios.create() as ApiService;
+const { HOST } = process.env;
 
 let interceptor: number;
 apiService.useService = (service: SERVICES): AxiosInstance => {
     if (interceptor) apiService.interceptors.request.eject(interceptor);
     interceptor = apiService.interceptors.request.use(req => {
-        const endpoint = process.env[`${SERVICES[service]}_RUNNING`] ? 'localhost' : 'recroute.co';
-        const prefix = `https://${endpoint}:${service}`;
+        const endpoint = process.env[`${SERVICES[service]}_RUNNING`] ? 'http://localhost' : 'https://recroute.co';
+        const prefix = `${endpoint}:${service}`;
         if (!req.url?.startsWith(prefix)) req.url = prefix + req.url;
         return req;
     });
